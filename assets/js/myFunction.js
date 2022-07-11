3e770e817fa16dd75846487b584d81ed0357ce46
GijsvandenBeucken <55687057+GijsvandenBeucken@users.noreply.github.com>
2022-07-11 11:51:48 +0200

    Update myFunction.js

--- assets/js/myFunction.js
+++ assets/js/myFunction.js
@@ -12,18 +12,5 @@ function myFunction() {
       btnText.innerHTML = "Minder"; 
       moreText.style.display = "inline";
     }
-    var dots2 = document.getElementById("dots2");
-    var moreText2 = document.getElementById("more2");
-    var btnText2 = document.getElementById("myBtn2");
-  
-    if (dots2.style.display === "none") {
-      dots2.style.display = "inline";
-      btnText2.innerHTML = "Meer info"; 
-      moreText2.style.display = "none";
-    } else {
-      dots2.style.display = "none";
-      btnText2.innerHTML = "Minder"; 
-      moreText2.style.display = "inline";
-    }
 
   }
